7a2e08a497e9e304eacb0e9a919d67506ca18110
Kadet <techkadet@gmail.com>
2020-11-01 17:44:09 +0100

    added credits

--- src/App.js
+++ src/App.js
@@ -1,105 +1,201 @@
 // import React from 'react'
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Switch } from 'react-router';
-import './App.css'
-import Base from './components/base/Base'
-import Work from './components/work/Work'
-import Resume from './components/resume/Resume'
-import Contact from './components/contact/Contact'
-import Mobile from './components/base/Mobile'
-import Header from './components/utils/header/Header'
-import Error from './components/error404/Error'
-
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import "./App.css";
+import Base from "./components/base/Base";
+import Work from "./components/work/Work";
+import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
+import Mobile from "./components/base/Mobile";
+import Header from "./components/utils/header/Header";
+import Error from "./components/error404/Error";
 
 export class App extends Component {
-
   state = {
-    menu: ['active_menu', '', ''],
-  }
+    menu: ["active_menu", "", ""],
+  };
 
   activeMenu(id) {
-
     switch (id) {
       case 0:
-        this.setState({menu: ['active_menu', '', '']})
+        this.setState({ menu: ["active_menu", "", ""] });
         break;
-      
+
       case 1:
-        this.setState({menu: ['', 'active_menu', '']})
+        this.setState({ menu: ["", "active_menu", ""] });
         break;
-      
-      case 2: 
-        this.setState({menu: ['', '', 'active_menu']})
-        break
-    
+
+      case 2:
+        this.setState({ menu: ["", "", "active_menu"] });
+        break;
+
       default:
         break;
     }
- 
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Router>
-
           <div className='mobile_container'>
             <Header />
             <div>
               <Switch>
-                <Route exact path='/' render={props => (<Mobile menu={this.state.menu} />)} />
+                <Route
+                  exact
+                  path='/'
+                  render={(props) => <Mobile menu={this.state.menu} />}
+                />
 
-                <Route exact path='/work' render={props => (<Work menu={this.state.menu} activeMenu={this.activeMenu.bind(this)} />)} />
+                <Route
+                  exact
+                  path='/work'
+                  render={(props) => (
+                    <Work
+                      menu={this.state.menu}
+                      activeMenu={this.activeMenu.bind(this)}
+                    />
+                  )}
+                />
 
-                <Route exact path='/resume' render={props => (<Resume menu={this.state.menu} activeMenu={this.activeMenu.bind(this)} />)}/>
+                <Route
+                  exact
+                  path='/resume'
+                  render={(props) => (
+                    <Resume
+                      menu={this.state.menu}
+                      activeMenu={this.activeMenu.bind(this)}
+                    />
+                  )}
+                />
 
-                <Route exact path='/contact' render={props => (<Contact menu={this.state.menu} activeMenu={this.activeMenu.bind(this)} />)} />
+                <Route
+                  exact
+                  path='/contact'
+                  render={(props) => (
+                    <Contact
+                      menu={this.state.menu}
+                      activeMenu={this.activeMenu.bind(this)}
+                    />
+                  )}
+                />
 
                 <Route component={Error} />
               </Switch>
             </div>
 
-            <div className="social_buttons">
-              <a href="https://github.com/kadetXx" target='_blank' rel="noopener noreferrer"><i className='fab fa-github'></i></a> 
-              <a href="https://twitter.com/kadetXx" target='_blank' rel="noopener noreferrer"><i className='fab fa-twitter'></i></a>
-              <a href="https://www.linkedin.com/in/collins-enebeli-online/" target='_blank' rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+            <div className='social_buttons'>
+              <a
+                href='https://github.com/kadetXx'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-github'></i>
+              </a>
+              <a
+                href='https://twitter.com/kadetXx'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-twitter'></i>
+              </a>
+              <a
+                href='https://www.linkedin.com/in/collins-enebeli-online/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-linkedin'></i>
+              </a>
+
+              <div className='credits'>
+                <p>
+                  Inspired by
+                  <a
+                    href='https://sarahdayan.dev/'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    Sarah Dayan.
+                  </a>
+                </p>
+
+                <p>
+                  Design by
+                  <a
+                    href='https://dribbble.com/NicolasMzrd'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    Nicolas Meuzard.
+                  </a>
+                </p>
+              </div>
+            </div>
           </div>
-          </div>
 
-
-
-          <div className="container">
-            <div className="fixed">
+          <div className='container'>
+            <div className='fixed'>
               <Base menu={this.state.menu} />
             </div>
 
-            <div className="main">
-  
-              <div className="pages_container">
+            <div className='main'>
+              <div className='pages_container'>
                 <Switch>
-                  <Route exact path='/' render={props => (<Work  menu={this.state.menu} activeMenu={this.activeMenu.bind(this)}/>)} />
+                  <Route
+                    exact
+                    path='/'
+                    render={(props) => (
+                      <Work
+                        menu={this.state.menu}
+                        activeMenu={this.activeMenu.bind(this)}
+                      />
+                    )}
+                  />
 
-                  <Route exact path='/work' render={props => (<Work  menu={this.state.menu} activeMenu={this.activeMenu.bind(this)}/>)} />
+                  <Route
+                    exact
+                    path='/work'
+                    render={(props) => (
+                      <Work
+                        menu={this.state.menu}
+                        activeMenu={this.activeMenu.bind(this)}
+                      />
+                    )}
+                  />
 
-                  <Route exact path='/resume' render={props => (<Resume menu={this.state.menu} activeMenu={this.activeMenu.bind(this)} />)} />
+                  <Route
+                    exact
+                    path='/resume'
+                    render={(props) => (
+                      <Resume
+                        menu={this.state.menu}
+                        activeMenu={this.activeMenu.bind(this)}
+                      />
+                    )}
+                  />
 
-                  <Route exact path='/contact' render={props => (<Contact menu={this.state.menu} activeMenu={this.activeMenu.bind(this)} />)} />
+                  <Route
+                    exact
+                    path='/contact'
+                    render={(props) => (
+                      <Contact
+                        menu={this.state.menu}
+                        activeMenu={this.activeMenu.bind(this)}
+                      />
+                    )}
+                  />
 
                   <Route component={Error} />
                 </Switch>
               </div>
-
             </div>
           </div>
-
         </Router>
       </div>
-    )
+    );
   }
-
 }
 
-export default App
-
+export default App;
--- src/components/base/Base.jsx
+++ src/components/base/Base.jsx
@@ -1,68 +1,140 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './Base.css'
-import avi from '../../assets/svgs/avi.svg'
-import navLine from '../../assets/svgs/menu-line.svg'
-import logo from '../../assets/svgs/kadet_footer.svg'
-import arrow from '../../assets/svgs/arrow.svg'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Base.css";
+import avi from "../../assets/svgs/avi.svg";
+import navLine from "../../assets/svgs/menu-line.svg";
+import logo from "../../assets/svgs/avismall.svg";
+import arrow from "../../assets/svgs/arrow.svg";
 
 export class Base extends Component {
-
   render() {
-
     return (
-      
       <div className='base_container'>
-        <div className="hero">
-          <div className="intro_box">
-            <h1> Hello! I'm  <br/>  Collins Enebeli.</h1>
-            <div className='avi'><img src={avi} alt="avatar"/></div>
+        <div className='hero'>
+          <div className='intro_box'>
+            <h1>
+              {" "}
+              Hello! I'm <br /> Collins Enebeli.
+            </h1>
+            <div className='avi'>
+              <img src={avi} alt='avatar' />
+            </div>
           </div>
 
-          <div className="description">
-            <p> <span>I'm a <span className='highlight'>Frontend Developer</span>. I spend most of my time writing code for applications using </span> <span className='highlight'>Javascript</span><span> and react js. I use CSS and </span> <span className='highlight'>SASS </span> <span> to design efficient user interfaces. I love talking about tech and also write on my <a href="https://kadet.hashnode.dev" target='_blank' rel="noopener noreferrer">blog</a> ocassionally</span></p>
+          <div className='description'>
+            <p>
+              {" "}
+              <span>
+                I'm a <span className='highlight'>Frontend Developer</span>. I
+                spend most of my time writing code for applications using{" "}
+              </span>{" "}
+              <span className='highlight'>Javascript</span>
+              <span> and react js. I use CSS and </span>{" "}
+              <span className='highlight'>SASS </span>{" "}
+              <span>
+                {" "}
+                to design efficient user interfaces. I love talking about tech
+                and also write on my{" "}
+                <a
+                  href='https://kadet.hashnode.dev'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  blog
+                </a>{" "}
+                ocassionally
+              </span>
+            </p>
           </div>
 
-          <div className="go_button_container">
-            <Link to='/work'> <button className='explore'>Explore ➜</button></Link>
+          <div className='go_button_container'>
+            <Link to='/work'>
+              {" "}
+              <button className='explore'>Explore ➜</button>
+            </Link>
           </div>
-
         </div>
-  
 
         <nav>
           <ul>
             <li className={this.props.menu[0]}>
-              <small>00  <Link to='/'><img src={navLine} alt="menu bullet"/> WORK</Link></small>
+              <small>
+                00{" "}
+                <Link to='/'>
+                  <img src={navLine} alt='menu bullet' /> WORK
+                </Link>
+              </small>
             </li>
 
             <li className={this.props.menu[1]}>
-              <small>01  <Link to='/resume'><img src={navLine} alt="menu bullet"/> RESUME</Link></small>
+              <small>
+                01{" "}
+                <Link to='/resume'>
+                  <img src={navLine} alt='menu bullet' /> RESUME
+                </Link>
+              </small>
             </li>
 
             <li className={this.props.menu[2]}>
-            <small>02 <Link to='/contact'><img src={navLine} alt="menu bullet"/> CONTACT</Link></small>
+              <small>
+                02{" "}
+                <Link to='/contact'>
+                  <img src={navLine} alt='menu bullet' /> CONTACT
+                </Link>
+              </small>
             </li>
           </ul>
         </nav>
 
-        <div className="footer">
-          <img src={logo} alt=""/>
-          <div className="social">
-
-            <a href='https://github.com/kadetXx' target='_blank' rel="noopener noreferrer"><i className='fab fa-github'></i> Github 
-            <img src={arrow} alt="pointer"/>
+        <div className='footer'>
+          <img src={logo} alt='' />
+          <div className='social'>
+            <a
+              href='https://github.com/kadetXx'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-github'></i> Github
+              <img src={arrow} alt='pointer' />
             </a>
 
-            <a href='https://twitter.com/kadetXx' target='_blank' rel="noopener noreferrer"><i className='fab fa-twitter'></i> Twitter <img src={arrow} alt="pointer"/>
+            <a
+              href='https://twitter.com/kadetXx'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-twitter'></i> Twitter{" "}
+              <img src={arrow} alt='pointer' />
             </a>
+          </div>
 
+          <div className='credits'>
+            <p>
+              Inspired by
+              <a
+                href='https://sarahdayan.dev/'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                Sarah Dayan.
+              </a>
+            </p>
+
+            <p>
+              Design by
+              <a
+                href='https://dribbble.com/NicolasMzrd'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                Nicolas Meuzard.
+              </a>
+            </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
-
 }
 
-export default Base
+export default Base;
