cd3696a9ee2f80c03fa15d97fe45c1992e745c44
kadet <techkadet@gmail.com>
2021-05-10 02:23:54 +0100

    added seo to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 
 import Error from "../components/error404/Error";
+import Seo from '../shared/seo/Seo'
 
 // markup
 const NotFoundPage = () => {
-  return <Error />;
+  return (
+    <React.Fragment>
+      <Seo title="404" />
+      <Error />
+    </React.Fragment>
+  );
 };
 
 export default NotFoundPage;
