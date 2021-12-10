43b678be1720cb82e43fcdb8116976bf5b7eb958
kadetXx <techkadet@gmail.com>
2021-12-10 16:23:04 +0100

    minors

--- src/shared/cursor/Cursor.jsx
+++ src/shared/cursor/Cursor.jsx
@@ -1,15 +1,33 @@
-import React, { useRef, useLayoutEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import "./Cursor.css";
 
 const Cursor = ({ children }) => {
-  const cursor = useRef(null);
+  const cursorRef = useRef(null);
+  const cursorIIRef = useRef(null);
 
-  useLayoutEffect(() => {
-    console.log("waddup btch");
+  useEffect(() => {
+    const cursor = cursorRef.current;
+    const cursorII = cursorIIRef.current;
+
+    const trail = (ev) => {
+      cursor.setAttribute(
+        "style",
+        `transform: translate3d(${ev.pageX - 10}px, ${ev.pageY - 10}px, 0px)`
+      );
+
+      cursorII.setAttribute(
+        "style",
+        `transform: translate3d(${ev.pageX - 3}px, ${ev.pageY - 3}px, 0px)`
+      );
+    };
+
+    document.addEventListener("mousemove", trail);
   }, []);
+
   return (
     <>
-      <div ref={cursor} className="custom-cursor"></div>
+      <div ref={cursorRef} className="custom-cursor"></div>
+      <div ref={cursorIIRef} className="custom-cursor-ii"></div>
       {children}
     </>
   );
