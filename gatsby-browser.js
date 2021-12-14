46d13f35fa470d94bcd94f89090fe9dce9d2daf1
kadetXx <techkadet@gmail.com>
2021-12-14 17:28:33 +0100

    config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,11 @@ import "./src/css/global.css";
 import React from "react";
 import CustomCursor from "./src/shared/cursor/Cursor";
 
+import {
+  PrismicPreviewProvider,
+  componentResolverFromMap,
+} from "gatsby-plugin-prismic-previews";
+
 export const wrapPageElement = ({ element, props }) => {
   return (
     <PrismicPreviewProvider
