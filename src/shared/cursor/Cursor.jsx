f71a1a9a17d99bfb509f6a28697047db32c4489f
kadetXx <techkadet@gmail.com>
2021-12-14 14:55:55 +0100

    edit cursor

--- src/shared/cursor/Cursor.jsx
+++ src/shared/cursor/Cursor.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import "./Cursor.css";
 
-const Cursor = ({ children, location }) => {
+const Cursor = ({ children }) => {
   // const cursorRef = useRef(null);
   const cursorIIRef = useRef(null);
 
@@ -9,32 +9,32 @@ const Cursor = ({ children, location }) => {
     // const cursor = cursorRef.current;
     const cursorII = cursorIIRef.current;
 
-    const path = location.pathname;
-    const isBlog = /\/blog\/\S*/gm.test(path)
+    // const path = location.pathname;
+    // const isBlog = /\/blog\/\S*/gm.test(path)
 
     // trailing cursor function
     const trail = (ev) => {
-      if (isBlog) {
-        // cursor.setAttribute("style", `display: none`);
-        cursorII.setAttribute("style", `display: none`);
-      } else {
-        // cursor.setAttribute(
-        //   "style",
-        //   `transform: translate3d(${ev.pageX - 10}px, ${ev.pageY - 10}px, 0px)`
-        // );
+      // if (isBlog) {
+      //   // cursor.setAttribute("style", `display: none`);
+      //   cursorII.setAttribute("style", `display: none`);
+      // } else {
+      // cursor.setAttribute(
+      //   "style",
+      //   `transform: translate3d(${ev.pageX - 10}px, ${ev.pageY - 10}px, 0px)`
+      // );
 
-        cursorII.setAttribute(
-          "style",
-          `transform: translate3d(${ev.pageX - 3}px, ${ev.pageY - 3}px, 0px)`
-        );
-      }
+      cursorII.setAttribute(
+        "style",
+        `transform: translate3d(${ev.pageX - 3}px, ${ev.pageY - 3}px, 0px)`
+      );
+      // }
     };
 
     // add event listener to activate trailing function
     document.addEventListener("mousemove", trail);
 
     return () => document.removeEventListener("mousemove", trail);
-  }, [location]);
+  }, []);
 
   return (
     <>
